776dae0e5a12014ad07160f956bc80ccc4c1e02f
Sergey Mokhov <SergeyMokhovMoss@gmail.com>
2018-06-20 08:00:11 -0400

    Created file for URL generator

---
+++ auth_url_generator.go
@@ -0,0 +1,12 @@
+package vkcli
+
+import (
+	"net/url"
+	"golang.org/x/oauth2/vk"
+)
+
+func NewAuthUrl(clientId string, redirectUri url.URL, ) (authUrl url.URL) {
+	//todo Finish the method. Maybe move stuff to auth package
+	//authUrl := url.Parse(vk.Endpoint.AuthURL)
+	return
+}

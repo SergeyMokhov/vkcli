2871dcd0ef81e5dc8088d2a5b8d465cba726ea2b
Sergey Mokhov <smokhov@pelmorex.com>
2018-08-31 15:43:17 -0400

    Added methods to set user id and order

--- api/requests/friends/friends.go
+++ api/requests/friends/friends.go
@@ -3,22 +3,41 @@ package friends
 import (
 	"fmt"
 	"net/url"
+	"strconv"
 )
 
-const methodBase string = "friends."
+const (
+	methodBase string         = "friends."
+	Hints      OrderOfFriends = "hints"
+	Random     OrderOfFriends = "random"
+	Mobile     OrderOfFriends = "mobile"
+	Name       OrderOfFriends = "name"
+)
 
 type friendsGetRequest struct {
 	values url.Values
 }
 
-func (fg friendsGetRequest) UrlValues() url.Values {
+type OrderOfFriends string
+
+func (fg *friendsGetRequest) UrlValues() url.Values {
 	return fg.values
 }
 
-func (fg friendsGetRequest) Method() string {
+func (fg *friendsGetRequest) Method() string {
 	return fmt.Sprint(methodBase, "get")
 }
 
-func Get() friendsGetRequest {
-	return friendsGetRequest{values: url.Values{}}
+func (fg *friendsGetRequest) SetUserId(usrId int) *friendsGetRequest {
+	fg.values.Add("user_id", strconv.Itoa(usrId))
+	return fg
+}
+
+func (fg *friendsGetRequest) SetOrder(order OrderOfFriends) *friendsGetRequest {
+	fg.values.Add("order", string(order))
+	return fg
+}
+
+func Get() *friendsGetRequest {
+	return &friendsGetRequest{values: url.Values{}}
 }
--- api/requests/friends/friends_test.go
+++ api/requests/friends/friends_test.go
@@ -16,3 +16,15 @@ func TestFriendsGetRequest_UrlValues(t *testing.T) {
 	actual := fgr.UrlValues()
 	require.NotNil(t, actual)
 }
+
+func TestFriendsGetRequest_SetUserId(t *testing.T) {
+	fgr := Get().SetUserId(9000)
+	actual := fgr.UrlValues().Get("user_id")
+	require.EqualValues(t, "9000", actual)
+}
+
+func TestFriendsGetRequest_SetOrder(t *testing.T) {
+	fgr := Get().SetOrder(Name)
+	actual := fgr.UrlValues().Get("order")
+	require.EqualValues(t, "name", actual)
+}
